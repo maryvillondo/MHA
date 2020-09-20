7d4c70f362c49974d854c77317d32c726839a533
lezdegamo <lezly.degamo@gmail.com>
2020-09-20 11:10:49 +0800

    Revert "Update models.py"
    This reverts commit 3bbc9e3d1e130f65af2ec2a6fb0b5a09403d6a79.

--- dvd/models.py
+++ dvd/models.py
@@ -1,21 +1,3 @@
 from django.db import models
-from datetime import datetime
 
 # Create your models here.
-
-class DVD(models.Model):
-	genre = models.CharField(max_length = 255)
-	title = models.CharField(max_length = 150)
-	director_firstname = models.CharField(max_length = 150)
-	director_lastname = models.CharField(max_length = 150)
-	release_date = models.DateField(default = datetime.now())
-	cast = models.CharField(max_length = 255)
-	quantity = models.IntegerField()
-	price = models.FloatField()
-	dvd_cover1 = models.ImageField(upload_to = 'dvd', null = True)
-	dvd_cover2 = models.ImageField(upload_to = 'dvd', null = True)
-	dvd_cover3 = models.ImageField(upload_to = 'dvd', null = True)
-	date_registered = models.DateField(default = datetime.now())
-
-	class Meta:
-		db_table = "DVD"
