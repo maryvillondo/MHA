2dacd8cb84d1ef1bb05cb113055877e85ad234db
lezdegamo <lezly.degamo@gmail.com>
2020-09-20 11:10:45 +0800

    Revert "Update custom2.js"
    This reverts commit 8b3df0e36a95fc5654c628dbd984a64da69fcb07.

--- static/js/custom2.js
+++ static/js/custom2.js
@@ -2,34 +2,37 @@
 
 //DASHBOARD
 // Add Cast:
+// Create a "close" button and append it to each list item (cast)
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+// Click on a close button to hide the current list item (cast)
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 // Create a new list item (cast) when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("castInput").value;
   var t = document.createTextNode(inputValue);
-
-  // Add cast input to list
-  var castArrList = document.getElementById("castList").value.split(",");
-  if(castArrList[0] == ''){
-    castArrList[0] = inputValue;
-  }
-  else{
-    castArrList.push(inputValue);
-  }
-  document.getElementById("castList").value = castArrList;
-  document.getElementById("castList").innerHTML = castArrList;
-
   li.appendChild(t);
-  li.setAttribute("value",inputValue);
   if (inputValue === '') {
     alert("You must write something!");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("castInput").value = "";
-
-  // Create a "close" button and append it to each list item (cast)
-  var close = document.getElementsByClassName("close");
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
@@ -38,20 +41,7 @@ function newElement() {
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
       var div = this.parentElement;
-
-      // Click on a close button to hide the current list item (cast)
       div.style.display = "none";
-
-      //Delete cast from list
-      var castArrList = document.getElementById("castList").value.split(",");
-      for(i = 0; i < castArrList.length; i++){ 
-        if ( castArrList[i] == this.parentElement.getAttribute("value")) { 
-          castArrList.splice(i, 1); 
-          i--; 
-        }
-      }
-      document.getElementById("castList").value = castArrList;
-      document.getElementById("castList").innerHTML = castArrList;
     }
   }
 }
@@ -154,10 +144,6 @@ var loadImgPreviewCustReg = function(event) {
 
 
 
-
-
-
-
 $(document).ready(function() {
   //DASHBOARD
   // Date range filter datepicker
@@ -188,7 +174,7 @@ $(document).ready(function() {
       'orderable': false, // set orderable false for vud
     }]
   } );
-
+  
   // Extend dataTables search
   $.fn.dataTable.ext.search.push(
     function(settings, data, dataIndex) {
@@ -207,38 +193,27 @@ $(document).ready(function() {
     table.draw();
   });
 
-  // Birthday   customer modal
+  // Birthday datepicker customer modal
   $('#datepickerBdayCustModal').datepicker({
-    uiLibrary: 'bootstrap4',
-    format: 'yyyy-mm-dd'
+    uiLibrary: 'bootstrap4'
   });
 
   // Release date datepicker
   $('#datepickerRelDateDvdModal').datepicker({
-    uiLibrary: 'bootstrap4',
-    format: 'yyyy-mm-dd'
+    uiLibrary: 'bootstrap4'
   });
-
-  
-
-
 
   //DVD REGISTRATION
   //Release date datepicker dvd registration
   $('#datepickerRelDateDvdReg').datepicker({
-    uiLibrary: 'bootstrap4',
-    format: 'yyyy-mm-dd'
+    uiLibrary: 'bootstrap4'
   });
-
-
-
-
 
   //CUSTOMER REGISTRATION
   //Birthday datepicker customer registration
   $('#datepickerBdayCustReg').datepicker({
     uiLibrary: 'bootstrap4',
-    format: 'yyyy-mm-dd'
+    format: "yyyy-mm-dd  "
   });
 } );
 
