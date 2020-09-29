96cece418a9f4abdc4ba28fb67f1b266744fd812
lezdegamo <lezly.degamo@gmail.com>
2020-09-29 13:17:53 +0800

    Added dvd update & delete functionalities

--- dvd/views.py
+++ dvd/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.http import HttpResponse
 from .forms import DVDForm
@@ -13,9 +13,42 @@ class DVDIndexView(View):
 		context = {
 			'dvds' : qs_dvds
 		}
-		return render(request, 'index.html', context)
+		return render(request, 'dashboard.html', context)
 	def post(self, request):
-		return render(request, 'DVDRegistration.html')
+		if request.method == 'POST':	
+			if 'btnUpdate' in request.POST:	
+				sku = request.POST.get("sku")
+				genrelist = request.POST.getlist("genre")
+				genre = ','.join(genrelist)
+				title = request.POST.get("title")
+				director_firstname = request.POST.get("director_firstname")
+				director_lastname = request.POST.get("director_lastname")
+				release_date = request.POST.get("release_date")
+				cast = request.POST.get("cast")
+				quantity = request.POST.get("quantity")
+				price = request.POST.get("price")
+				# dvd_cover1 = request.FILES.get("dvd_cover1")
+				# dvd_cover2 = request.FILES.get("dvd_cover2")
+				# dvd_cover3 = request.FILES.get("dvd_cover3")
+				update_dvd = DVD.objects.filter(id = sku).update(genre = genre, 
+																	title = title, 
+																	director_firstname = director_firstname, 
+																	director_lastname = director_lastname, 
+																	release_date = release_date,
+																	cast = cast, 
+																	quantity = quantity, 
+																	price = price)
+				# if dvd_cover1 is not None:
+				# 	update_dvd_cover1 = DVD.objects.filter(id = sku).update(dvd_cover1 = dvd_cover1)
+				# if dvd_cover2 is not None:
+				# 	update_dvd_cover2 = DVD.objects.filter(id = sku).update(dvd_cover2 = dvd_cover2)
+				# if dvd_cover3 is not None:
+				# 	update_dvd_cover3 = DVD.objects.filter(id = sku).update(dvd_cover3 = dvd_cover3)
+			elif 'btnDelete' in request.POST:	
+				sku = request.POST.get("sku")
+				dvd = DVD.objects.filter(id = sku).delete()
+		#return HttpResponse ('post')
+		return redirect('dvd:dvd_index_view')
 
 class DVDRegistrationView(View):
 	def get(self, request):
